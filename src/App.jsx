e0258ce46a2601ad6b0939db026a4d02cdf03b39
kenzie <kenkei1913@gmail.com>
2026-01-02 20:19:49 +0800

    Added form submission functionality using Web3Forms. Form submission is sent to my email

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,23 @@ import Services from './components/Services'
 import OurWork from './components/OurWork'
 import Teams from './components/Teams'
 import ContactUs from './components/ContactUs'
+import { Toaster } from 'react-hot-toast' 
 
 const App = () => {
 
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
   return (
-      <div className='dark:bg-black relative'>
-          <Navbar theme={theme} setTheme={setTheme}/>
-          <Hero />
-          <TrustedBy />
-          <Services />
-          <OurWork />
-          <Teams />
-          <ContactUs />
-      </div>
-  )
+        <div className='dark:bg-black relative'>
+            <Toaster />
+            <Navbar theme={theme} setTheme={setTheme}/>
+            <Hero />
+            <TrustedBy />
+            <Services />
+            <OurWork />
+            <Teams />
+            <ContactUs />
+        </div>
+    )
 }
 
 export default App
